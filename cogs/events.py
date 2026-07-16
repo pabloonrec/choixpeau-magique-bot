4e3cb94409efe3962cf46f501ed180c4ef472791
pabloonrec <alexisdeclunder@gmail.com>
2026-07-16 19:59:33 +0200

    Create events.py

---
+++ cogs/events.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+
+class Events(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("📜 Events chargés.")
+
+async def setup(bot):
+    await bot.add_cog(Events(bot))
