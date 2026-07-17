6a00b278daed3c2abca5a67fec8232f6789db39a
pabloonrec <alexisdeclunder@gmail.com>
2026-07-17 16:04:47 +0200

    Delete cogs directory

--- cogs/choixpeau.py
+++
@@ -1,8 +0,0 @@
-from discord.ext import commands
-
-class Choixpeau(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-async def setup(bot):
-    await bot.add_cog(Choixpeau(bot))
